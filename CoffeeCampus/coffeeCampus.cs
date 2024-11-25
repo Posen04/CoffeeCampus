9660b4b829664bdfc71f53b47f9ed8a6430a63f6
Sophu <Sophu@POSEN>
2024-11-25 12:12:34 +0100

    Add project files.

---
+++ CoffeeCampus/coffeeCampus.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.EntityFrameworkCore;
+
+namespace CoffeeCampus
+{
+	public class coffeeCampus : IdentityDbContext
+	{
+        public coffeeCampus(DbContextOptions options) 
+
+        {
+
+		}
+
+		// Definer dine DbSets her
+		//public DbSet<> YourEntities { get; set; }
+	}
+}
